5023303fe158424e233f495182e7e97f39e82af9
stanou <stan.demontmarin@gmail.com>
2023-08-22 11:49:59 +0200

    new structure

--- projetfinal/main.c
+++ projetfinal/main.c
@@ -2,8 +2,26 @@
 #include "struct.h"
 #include "func.h"
 
+void create_bat(Hotel_de_ville hdv, int numero_batiment)
+{
+    switch (numero_batiment)
+    {
+    case 1:
+        if(hdv.or>=50)
+        {
+            hdv.total_scierie++;
+            hdv.or-=50;
+
+        }
+        break;
+    
+    default:
+        break;
+    }
+}
+
 int main()
 {
-    
+    Hotel_de_ville
     return 0;
 }
--- projetfinal/struct.h
+++ projetfinal/struct.h
@@ -4,17 +4,17 @@
     typedef struct 
     {
         int level; // niveau de l'hotel de ville
-        scierie *liste_scierie;
-        mine *liste_mine;
-        raffinerie *liste_raffinerie;
+        Scierie *liste_scierie;
+        Mine *liste_mine;
+        Raffinerie *liste_raffinerie;
         int total_scierie;
         int total_mine;
         int total_raffinerie;
-        int bois, oir, mat_noire; // compteur ressources
+        int bois, or, mat_noire; // compteur ressources
         int max_peons; // Maximum de peons disponibles à un lvl donné
         int peons_disponibles; // total de peons au chomage
         int total_soldats; 
-    }hotel_de_ville;
+    }Hotel_de_ville;
     
     typedef struct 
     {
@@ -22,7 +22,8 @@
         int cout_bois;
         int cout_mat_noire;
         int production_j;
-        int peons;
-    }scierie, mine, raffinerie;
+        int peons_necessaires;
+        int peons_assignés;
+    }Scierie, Mine, Raffinerie;
     
 #endif
