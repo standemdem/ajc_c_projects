96ef482496072bdc9a10c1a9a0aa796508a402c9
stanou <stan.demontmarin@gmail.com>
2023-08-22 12:02:16 +0200

    push

--- projetfinal/func.c
+++ projetfinal/func.c
@@ -1,13 +1,45 @@
 #include "lib.h"
+#include "struct.h"
 
 void afficher_etat_village()
 {
 
 }
 
-void construire_batiment()
+void create_bat(Hotel_de_ville hdv, int numero_batiment)
 {
+    switch (numero_batiment)
+    {
+    case 1: // scierie
+        if(hdv.or>=50)
+        {
+            hdv.total_scierie++;
+            hdv.or-=50;
 
+        }
+        break;
+    case 2: // Mine
+        if(hdv.or >= 100 && hdv.bois >= 20)
+        {
+            hdv.total_mine++;
+            hdv.or -= 100;
+            hdv.bois -= 20;
+
+        }
+        break;    
+    case 3: // Rafinerie
+        if(hdv.or >= 500 && hdv.bois >=100, hdv.mat_noire>=50)
+        {
+            hdv.total_raffinerie++;
+            hdv.or -= 500;
+            hdv.bois -= 100;
+            hdv.mat_noire -= 50;
+
+        }
+        break;
+    default:
+        break;
+    }
 }
 
 void assigner_villageois()
@@ -17,5 +49,5 @@ void assigner_villageois()
 
 void collecter_ressources()
 {
-    
+
 }
