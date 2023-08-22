e2dfacc88bb39781b7141cda9f61b89b4307b433
stanou <stan.demontmarin@gmail.com>
2023-08-22 11:27:33 +0200

    changed structure of hdv

--- projetfinal/struct.h
+++ projetfinal/struct.h
@@ -3,11 +3,17 @@
 
     typedef struct 
     {
+        int level; // niveau de l'hotel de ville
         scierie *liste_scierie;
         mine *liste_mine;
         raffinerie *liste_raffinerie;
-        int max_peons;
-        int peons_disponibles;
+        int total_scierie;
+        int total_mine;
+        int total_raffinerie;
+        int bois, oir, mat_noire; // compteur ressources
+        int max_peons; // Maximum de peons disponibles à un lvl donné
+        int peons_disponibles; // total de peons au chomage
+        int total_soldats; 
     }hotel_de_ville;
     
     typedef struct 
