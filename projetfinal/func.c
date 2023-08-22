d99734039f158d5a23cf4dd6bc839c15f5015dd0
stanou <stan.demontmarin@gmail.com>
2023-08-22 11:15:22 +0200

    added empty functions

---
+++ projetfinal/func.c
@@ -0,0 +1,21 @@
+#include "lib.h"
+
+void afficher_etat_village()
+{
+
+}
+
+void construire_batiment()
+{
+
+}
+
+void assigner_villageois()
+{
+
+}
+
+void collecter_ressources()
+{
+    
+}
--- projetfinal/struct.h
+++ projetfinal/struct.h
@@ -1,11 +1,21 @@
 #ifndef STRUCT_H
 #define STRUCT_H
+
+    typedef struct 
+    {
+        scierie *liste_scierie;
+        mine *liste_mine;
+        raffinerie *liste_raffinerie;
+        int max_peons;
+        int peons_disponibles;
+    }hotel_de_ville;
+    
     typedef struct 
     {
         int cout_or;
         int cout_bois;
         int cout_mat_noire;
-        int production;
+        int production_j;
         int peons;
     }scierie, mine, raffinerie;
     
