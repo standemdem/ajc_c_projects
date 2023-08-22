e4612938e347620dfdaf4b096fdca6e09d872d8f
stanou <stan.demontmarin@gmail.com>
2023-08-22 11:02:26 +0200

    commit

--- projetfinal/func.h
+++ projetfinal/func.h
@@ -1,4 +1,5 @@
 #ifndef FUNC_H
 #define FUNC_H
     #include "lib.h"
+    #include "struct.h"
 #endif
--- projetfinal/struct.h
+++ projetfinal/struct.h
@@ -1,4 +1,12 @@
 #ifndef STRUCT_H
 #define STRUCT_H
+    typedef struct 
+    {
+        int cout_or;
+        int cout_bois;
+        int cout_mat_noire;
+        int production;
+        int peons;
+    }scierie, mine, raffinerie;
     
 #endif
